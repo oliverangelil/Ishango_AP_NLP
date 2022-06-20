d95d24afe7ba704c730461769f72309ffac68df5
a-stephen <sadjignon015@gmail.com>
2022-06-20 14:12:07 +0000

    feat: fetch articles from the database

--- connect_neo4j.py
+++ connect_neo4j.py
@@ -1,3 +1,4 @@
+
 from neo4j import GraphDatabase
 from collections import Counter
 import itertools
@@ -23,34 +24,85 @@ class Neo4jConnect:
         else:
             self.driver.close()
 
+    def __unpackResults__(self, items2unpack: list):
+        """
+            Helper fucntion:
+                unpack a list of lists into list
+        """
+        return list(itertools.chain(*items2unpack))
+
+    def __getCounter__(self, items2count: list):
+        """
+            Helper function:
+                count unique items in a list and return a dictionary
+        """
+        return dict(Counter(items2count))
+
     def __getEntity__(self):
+        """
+            Returns a dictionary:
+                key: database entity (node)
+                value: number of occurence
+        """
         query = "MATCH (n) RETURN labels(n)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
                 node_results = [dict(ii)['labels(n)'][0] for ii in result]
-                nodeLabels = Counter(node_results)
         else:
             print("No connected to Neo4j")
-        return dict(nodeLabels)
+        return self.__getCounter__(node_results)
 
     def __getRelations__(self):
+        """
+            return a dictionary:
+                key: database relationship type
+                value: number of occurence
+        """
         query = "MATCH p=()-[r]-() RETURN TYPE(r)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
-                relations = Counter([dict(i)['TYPE(r)'] for i in result])
+                relations = [dict(i)['TYPE(r)'] for i in result]
         else:
             pass
-        return dict(relations)
+        return self.__getCounter__(relations)
 
     def __getProperties__(self):
+        """
+            return a dictionary:
+                key: database properties key
+                value: number of occurence
+        """
         query = "MATCH (n) RETURN KEYS(n)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
                 props_results = [dict(i)['KEYS(n)'] for i in result]
-                props_keys = Counter(list(itertools.chain(*props_results)))
+                props_keys = self.__unpackResults__(props_results)
         else:
             pass
-        return dict(props_keys)
+        return self.__getCounter__(props_keys)
+
+    def getArticleBody(self):
+        """
+            Returns a dictionary:
+                key: author_name
+                value: dictionary (datePublished: mainArticle)
+        """
+        if self.driver is not None:
+            # entity_res = self.__getEntity__()
+            query = """
+                    match (n: Article)
+                    return n.headline, n.articleBody,
+                    n.author, n.probability,
+                    n.datePublished"""
+            with self.driver.session() as session:
+                ent_props = session.run(query)
+                ent_props = [dict(i) for i in ent_props]
+                articles = {
+                    f"{i['n.author']}":
+                    {'date': i['n.datePublished'],
+                     'mainArticle': i['n.articleBody']}
+                    for i in ent_props}
+        return articles
