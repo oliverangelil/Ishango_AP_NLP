6b7447be0c40c1e2eb1ac21d423e21934dc72c4e
a-stephen <sadjignon015@gmail.com>
2022-06-20 08:54:20 +0000

    fix: conforms with flake8 rules

--- connect_neo4j.py
+++ connect_neo4j.py
@@ -2,6 +2,7 @@ from neo4j import GraphDatabase
 from collections import Counter
 import itertools
 
+
 class Neo4jConnect:
     def __init__(self, uri, user, password):
         self.db_uri = uri
@@ -11,42 +12,45 @@ class Neo4jConnect:
         self.driver = None
 
         try:
-            self.driver = GraphDatabase.driver(self.db_uri, auth=(self.db_user, self.db_pass))
+            self.driver = GraphDatabase.driver(
+                self.db_uri, auth=(self.db_user, self.db_pass))
         except Exception as err:
             print("Failed to connect to Database: ", err)
-    
-    def closeConnection(self):
-        if self.driver is None: pass
-        else: self.driver.close()        
 
-    def getEntity(self):
+    def __closeConnection__(self):
+        if self.driver is None:
+            pass
+        else:
+            self.driver.close()
+
+    def __getEntity__(self):
         query = "MATCH (n) RETURN labels(n)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
-                nodeLabels = Counter([dict(ii)['labels(n)'][0] for ii in result])
-        else: print("No connected to Neo4j")
+                node_results = [dict(ii)['labels(n)'][0] for ii in result]
+                nodeLabels = Counter(node_results)
+        else:
+            print("No connected to Neo4j")
         return dict(nodeLabels)
-    
-    def getRelations(self):
+
+    def __getRelations__(self):
         query = "MATCH p=()-[r]-() RETURN TYPE(r)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
                 relations = Counter([dict(i)['TYPE(r)'] for i in result])
-        else: pass
+        else:
+            pass
         return dict(relations)
-    
-    def getProperties(self):
+
+    def __getProperties__(self):
         query = "MATCH (n) RETURN KEYS(n)"
         if self.driver is not None:
             with self.driver.session() as session:
                 result = session.run(query)
-                props_keys = Counter(list(itertools.chain(*[dict(i)['KEYS(n)'] for i in result])))
-        else: pass
+                props_results = [dict(i)['KEYS(n)'] for i in result]
+                props_keys = Counter(list(itertools.chain(*props_results)))
+        else:
+            pass
         return dict(props_keys)
-
-    def getArticles(self):
-        pass
-
-# Remove class implementation
